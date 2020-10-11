2610b4526552f6ca3ff33cf48abf787e640a3b4c
Filipe Lima <filipe.lima@rydoo.com>
2020-10-11 16:29:47 +0200

    fixing docker compose

--- src/Producer/Program.cs
+++ src/Producer/Program.cs
@@ -1,14 +1,18 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace Producer
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            while(true)
+            var counter = 0;
+            var max = args.Length != 0 ? Convert.ToInt32(args[0]) : -1;
+            while (max == -1 || counter < max)
             {
-                
+                Console.WriteLine($"Counter: {++counter}");
+                await Task.Delay(1000);
             }
         }
     }
